0151bc2653262b07f3b538700b29ea52846b56c7
unknown <olyasergeevna@list.ru>
2019-07-31 18:43:26 -0700

    Server

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+
+const indexRouter = require('./routes/index')
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/memory', { useNewUrlParser: true });
+
+var app = express();
+app.use(logger('dev'));
+
+const sessionConfig = {
+    secret: "keyboard cat",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore({}),
+}
+
+app.use(session(sessionConfig));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+module.exports = app;
