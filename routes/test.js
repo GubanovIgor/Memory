41f46d7f5ab0ad377739391f7ce561fcc67e7470
unknown <olyasergeevna@list.ru>
2019-08-01 12:16:05 -0700

    test2

--- routes/test.js
+++ routes/test.js
@@ -2,7 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test', function (req, res) {
-    res.render('test')
+    const answers = [
+        {number: 1},
+        {number: 2},
+        {number: 3},
+        {number: 4},
+        {number: 5},
+        {number: 6},
+        {number: 7},
+        {number: 8},
+        {number: 9},
+        {number: 10},
+        {number: 11},
+        {number: 12},
+        {number: 13},
+        {number: 14},
+        {number: 15},
+        {number: 16},
+        {number: 17},
+        {number: 18},
+        {number: 19},
+        {number: 20},
+    ]
+    res.render('test', {
+        answers: answers 
+    })
 })
+
+
 
 module.exports = router
