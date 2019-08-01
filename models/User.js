9ec2ad14d728b4fa427d6b505e020c87886fe127
krolkovAA <krolikovandrey@gmail.com>
2019-08-01 16:14:50 +0300

    statistick

--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost:27017/Avito", { useNewUrlParser: true })
+mongoose.connect("mongodb://localhost:27017/memory", { useNewUrlParser: true })
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-    firsname: String,
+    firstname: String,
     secondname: String,
     email: { type: String, unique: true }
 })
 
 const User = mongoose.model("User", userSchema);
 module.exports = {User, userSchema}
+// module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const Test = require('../models/Test')
+const { User } = require('../models/User')
+
+
+router.get('/', function (req, res) {
+    res.render('register');
+});
+
+
+router.get('/stat', function (req, res) {
+    res.render('stat');
+});
+
+router.post('/stat', async function (req, res, next) {
+    let user = await User.findOne({ email: req.body.email });
+    console.log(user._id);
+    let tests = await Test.find({ user: user._id })
+    console.log(tests)
+    // let countRight = tests.countRight
+    // let positionRight = tests.positionRight
+    // let total = tests.total
+    // console.log (countRight, positionRight, total)
+    
+    res.json(tests);
+
+});
 
 
 
 
 module.exports = router;
+
+
+// console.log(email)
+// let user = await User.find({ 'email': email })
+// let [findmail] = await Test.find({ user: { $eq: user } })
+// console.log('1', findmail)
+// const { countRight } = findmail
+// console.log('2', countRight)
