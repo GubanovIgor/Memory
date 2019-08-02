8fc8fefa7f0e0ddbde374ae63854b6bcc3201fc3
Anita <y_anita@mail.ru>
2019-08-02 18:17:31 +0300

    routerGet

--- models/Test.js
+++ models/Test.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose')
-const User = require('../models/User')
-const { userSchema } = require('./User')
-mongoose.connect("mongodb://localhost:27017/memory", { useNewUrlParser: true })
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const { userSchema } = require('./User');
+mongoose.connect('mongodb://localhost:27017/memory', { useNewUrlParser: true });
 const Schema = mongoose.Schema;
 const testSchema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref :'User'},
-    firstName: String,
-    secondName: String,
-    email: String,
-    words: [],
-    countRight: Number,
-    positionRight: Number,
-    total: String,
-})
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  firstName: String,
+  secondName: String,
+  email: String,
+  words: [],
+  countRight: Number,
+  positionRight: Number,
+  total: String
+});
 
-const Test = mongoose.model("Test", testSchema);
-module.exports = Test;
+const Test = mongoose.model('Test', testSchema);
+
+module.exports = { Test };
