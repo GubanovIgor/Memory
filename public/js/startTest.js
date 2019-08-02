098cddb4894223e26cf3c2d47c17de51af14168d
Gubanov Igor <applecake@inbox.ru>
2019-08-02 14:38:30 +0300

    соединил несоединяемое

--- public/js/startTest.js
+++ public/js/startTest.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  document.addEventListener('click', async function(e) {
+  document.addEventListener('click', async function (e) {
     if (e.target.classList.contains('start-test')) {
       e.preventDefault();
 
       const resp = await fetch('/sound', {
-        method:'POST',
+        method: 'POST',
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
@@ -21,5 +21,37 @@ document.addEventListener('DOMContentLoaded', (event) => {
       const container = document.querySelector('.container');
       container.innerHTML = templates.playSounds(data);
     }
-  });
+
+    // let resbutton = document.getElementById('start-voice')
+    // resbutton && resbutton.addEventListener('click', async (e) => {
+    //   e.preventDefault();
+      console.log('e.target');
+      // нажатие кнопки на бек
+      const reqback = await fetch(`/voice`, {
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify()
+      })
+
+      const { linkArr } = await reqback.json()
+
+      function sound() {
+        function plays(n) {
+          if (n < linkArr.length) {
+            setTimeout(() => {
+              var audio = new Audio(linkArr[n]);
+              audio.play();
+              n++
+              plays(n)
+            }, 2000);
+          }
+
+        }
+        plays(0)
+      }
+      sound()
+    })
+  // });
 });
