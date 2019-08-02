1281fc684a063c8c62f562dabd4a2384bafdd124
Gubanov Igor <applecake@inbox.ru>
2019-08-02 09:04:38 +0300

    убрал лишний роутер

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,6 @@ const router = express.Router();
 
 const { User } = require('../models/User');
 
-router.get('/register', function(req, res) {
-  res.render('register', {
-    instructions: res.instructions,
-    test: res.test,
-    stat: res.stat,
-  });
-
-});
-
 router.post('/addUser', async function(req, res, next) {
   const name = req.body.firstName;
   const surname = req.body.secondName;
