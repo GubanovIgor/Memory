d8168a0fb12c4d9aa9dfa7a5502cc6899ff5cd01
Gubanov Igor <applecake@inbox.ru>
2019-08-05 11:19:32 +0300

    получение номера порта из переменных окуржения

--- app.js
+++ app.js
@@ -50,8 +50,10 @@ app.use('/', voiceRouter);
 app.use('/', getRouter);
 // app.use('/', displayRouter);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
 
 module.exports = app;
